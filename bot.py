d1ce4f2d9f947b4b872b906cc25d1b0f85dbc635
Linerly <47475676+Linerly@users.noreply.github.com>
2021-05-30 21:11:06 +0700

    Added more slash commands from the existing bot commands.

--- bot.py
+++ bot.py
@@ -50,6 +50,23 @@ async def help(ctx, args = None):
 
     await ctx.send(embed = help_embed)
 
+@slash.slash(name = "help", description = "Shows a list of the bot's commands.")
+async def _help(ctx, args = None):
+    help_embed = discord.Embed(title = "Let me help you!", color = 0x1e90ff)
+    help_embed.set_author(name = "LinerlyBot", url = "https://linerly.github.io/linerlybot", icon_url = "https://linerly.github.io/assets/linerlybot/linerlybot.jpg")
+    command_names_list = [x.name for x in client.commands]
+
+    if not args:
+      help_embed.add_field(name = "Bot Commands", value = "\n".join([x.name for i, x in enumerate(client.commands)]), inline = False)
+      help_embed.add_field(name = "Details", value = "Type `l!help <command name>` for more details about each command. \n \n*This is a rewritten version of LinerlyBot using Discord.py.*", inline = False)
+
+    elif args in command_names_list:
+      help_embed.add_field(name = args, value = client.get_command(args).help)
+
+    else:
+      help_embed.add_field(name = "Invalid command!", value = "That command isn't available.")
+
+    await ctx.send(embed = help_embed)
 
 @client.command(help = "Tells you about the bot.")
 async def about(ctx):
@@ -58,6 +75,12 @@ async def about(ctx):
     embed.add_field(name = "A new version of LinerlyBot which uses Discord.py instead of Discord.js.", value = "Previous LinerlyBot commands will be added back in the rewritten version! \n \n [Website](https://linerly.github.io/linerlybot) \n [Add LinerlyBot to your Discord Server](https://discord.com/oauth2/authorize?client_id=529566778293223434&permissions=2147485696&scope=bot+applications.commands) \n [Source Code](https://github.com/Linerly/linerlybot-rewritten)")
     await ctx.send(embed = embed)
 
+@slash.slash(name = "about", description = "Tells you about the bot.")
+async def _about(ctx):
+    embed = discord.Embed(color = 0x1e90ff)
+    embed.set_author(name = "LinerlyBot", url = "https://linerly.github.io/linerlybot", icon_url = "https://linerly.github.io/assets/linerlybot/linerlybot.jpg")
+    embed.add_field(name = "A new version of LinerlyBot which uses Discord.py instead of Discord.js.", value = "Previous LinerlyBot commands will be added back in the rewritten version! \n \n [Website](https://linerly.github.io/linerlybot) \n [Add LinerlyBot to your Discord Server](https://discord.com/oauth2/authorize?client_id=529566778293223434&permissions=2147485696&scope=bot+applications.commands) \n [Source Code](https://github.com/Linerly/linerlybot-rewritten)")
+    await ctx.send(embed = embed)
 
 @client.command(help = "Checks the bot's current latency.")
 async def ping(ctx):
@@ -66,9 +89,20 @@ async def ping(ctx):
     embed.set_author(name = "LinerlyBot", url = "https://linerly.github.io/linerlybot", icon_url = "https://linerly.github.io/assets/linerlybot/linerlybot.jpg")
     await ctx.send(embed = embed)
 
+@slash.slash(name = "ping", description = "Checks the bot's current latency.")
+async def _ping(ctx):
+    ping = str(round(client.latency * 1000))
+    embed = discord.Embed(title = "Pong!", description = ping + " ms.", color = 0x1e90ff)
+    embed.set_author(name = "LinerlyBot", url = "https://linerly.github.io/linerlybot", icon_url = "https://linerly.github.io/assets/linerlybot/linerlybot.jpg")
+    await ctx.send(embed = embed)
 
 @client.command(help = "I can show you what I'm feeling now.")
 async def feeling(ctx):
+    feelings = [':slight_smile:', ':upside_down:', ':woozy_face:', ':confused:', ':sleeping:', ':rolling_eyes:', ':smiling_face_with_tear:', ':no_mouth:']
+    await ctx.send(random.choice(feelings))
+
+@slash.slash(name = "feeling", description = "I can show you what I'm feeling now.")
+async def _feeling(ctx):
     feelings = [':slight_smile:', ':upside_down:', ':woozy_face:', ':confused:', ':sleeping:', ':rolling_eyes:', ':smiling_face_with_tear:', ':no_mouth:']
     await ctx.send(random.choice(feelings))
 
@@ -111,8 +145,30 @@ async def balance(ctx):
     embed.add_field(name = "_ _", value = "Please be aware that new users that isn't in the database will result in an internal error!")
     await ctx.send(embed = embed)
 
+@slash.slash(name = "balance", description = "Check your balance by using the command.")
+async def _balance(ctx):
+    gold_balance = str(get_gold_balance(ctx.author))
+
+    embed = discord.Embed(title = "You currently have...", description = "<:gold:752147412445036645> " + gold_balance, color = 0x1e90ff)
+    embed.set_author(name = "LinerlyBot", url = "https://linerly.github.io/linerlybot", icon_url = "https://linerly.github.io/assets/linerlybot/linerlybot.jpg")
+    embed.add_field(name = "_ _", value = "Please be aware that new users that isn't in the database will result in an internal error!")
+    await ctx.send(embed = embed)
+
 @client.command(help = "Get some gold by working!")
 async def work(ctx):
+    job = "(insert job here)"
+    gold = random.randint(0, 150)
+
+    embed = discord.Embed(title = "Working", description = ctx.author.mention +", you work as a " + job + " and you got " + "<:gold:752147412445036645> " + str(gold) + " for working!", color = 0x1e90ff)
+    embed.set_author(name = "LinerlyBot", url = "https://linerly.github.io/linerlybot", icon_url = "https://linerly.github.io/assets/linerlybot/linerlybot.jpg")
+    embed.add_field(name = "_ _", value = "Please be aware that new users that isn't in the database will result in an internal error!")
+
+    add_gold_balance(ctx.author, get_gold_balance(ctx.author) + gold)
+
+    await ctx.send(embed = embed)
+
+@slash.slash(name = "work", description = "Get some gold by working!")
+async def _work(ctx):
     job = "(insert job here)"
     gold = random.randint(0, 150)
 
@@ -144,7 +200,31 @@ async def info(ctx):
     embed.add_field(name = "Operating System", value = ":penguin: running on " + str(distro.name(pretty = True)))
     await ctx.send(embed = embed)
 
-@slash.slash(name = "test", description = "A test command to check if slash commands works, nothing else.", guild_ids = guild_ids)
+@slash.slash(name = "info", description = "Shows some info about the bot.")
+async def _info(ctx):
+    current_time = time.time()
+    difference = int(round(current_time - start_time))
+    text = str(timedelta(seconds = difference))
+  
+    ping = str(round(client.latency * 1000))
+
+    embed = discord.Embed(title = "Bot Info", color = 0x1e90ff)
+    embed.set_author(name = "LinerlyBot", url = "https://linerly.github.io/linerlybot", icon_url = "https://linerly.github.io/assets/linerlybot/linerlybot.jpg")
+
+    embed.add_field(name = "Bot Uptime", value = ":green_circle: " + text)
+
+    embed.add_field(name = "Servers", value = ":desktop: in " + str(len(client.guilds)) + " servers")
+
+    embed.add_field(name = "Latency", value = ":globe_with_meridians: " + ping + " ms")
+
+    embed.add_field(name = "Operating System", value = ":penguin: running on " + str(distro.name(pretty = True)))
+    await ctx.send(embed = embed)
+
+@slash.slash(help = "A test command to check if the bot works, nothing else.")
+async def _test(ctx):
+    await ctx.send("test")
+
+@slash.slash(name = "test", description = "A test command to check if slash commands works, nothing else.")
 async def _test(ctx):
     await ctx.send("test")
 
