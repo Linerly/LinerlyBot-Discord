7cd86e51074ddc6f3784a028346744aff757c531
Linerly <47475676+Linerly@users.noreply.github.com>
2021-06-15 22:17:55 +0700

    Used `isort` and `black` to format code now.

--- bot.py
+++ bot.py
@@ -1,186 +1,299 @@
 print("Preparing the bot...")
-import discord
+import json
 import os
-import distro
-import time
 import random
+import time
+from datetime import timedelta
+
+import discord
+import distro
 import pyjokes
 import requests
-import json
-from webserver import keep_alive
-from quoters import Quote
-from datetime import timedelta
-from discord_slash import SlashCommand
 from discord.ext import commands
+from discord_slash import SlashCommand
+from quoters import Quote
 
-client = commands.Bot(command_prefix = "l!", help_command = None)
-slash = SlashCommand(client, sync_commands = True)
+from webserver import keep_alive
 
-client.remove_command('help')
+client = commands.Bot(command_prefix="l!", help_command=None)
+slash = SlashCommand(client, sync_commands=True)
+
+client.remove_command("help")
+
 
 @client.event
 async def on_ready():
     global start_time
     start_time = time.time()
 
-    activity = discord.Activity(name = "l!help - linerly.github.io/linerlybot", type = discord.ActivityType.playing)
-    await client.change_presence(status = discord.Status.online, activity = activity)
+    activity = discord.Activity(
+        name="l!help - linerly.github.io/linerlybot", type=discord.ActivityType.playing
+    )
+    await client.change_presence(status=discord.Status.online, activity=activity)
     print("I should be ready now!")
 
-@client.command(help = "Shows a list of the bot's commands.")
-async def help(ctx, command = None):
-    help_embed = discord.Embed(title = "Let me help you!", color = 0x1e90ff)
-    help_embed.set_author(name = "LinerlyBot", url = "https://linerly.github.io/linerlybot", icon_url = "https://linerly.github.io/assets/linerlybot/linerlybot.png")
+
+@client.command(help="Shows a list of the bot's commands.")
+async def help(ctx, command=None):
+    help_embed = discord.Embed(title="Let me help you!", color=0x1E90FF)
+    help_embed.set_author(
+        name="LinerlyBot",
+        url="https://linerly.github.io/linerlybot",
+        icon_url="https://linerly.github.io/assets/linerlybot/linerlybot.png",
+    )
     command_names_list = [x.name for x in client.commands]
 
     if not command:
-      help_embed.add_field(name = "Bot Commands", value = "\n".join([x.name for i, x in enumerate(client.commands)]), inline = False)
-      help_embed.add_field(name = "Details", value = "Type `l!help <command name>` for more details about each command. \n \n*This is a rewritten version of LinerlyBot using Discord.py.*", inline = False)
+        help_embed.add_field(
+            name="Bot Commands",
+            value="\n".join([x.name for i, x in enumerate(client.commands)]),
+            inline=False,
+        )
+        help_embed.add_field(
+            name="Details",
+            value="Type `l!help <command name>` for more details about each command. \n \n*This is a rewritten version of LinerlyBot using Discord.py.*",
+            inline=False,
+        )
 
     elif command in command_names_list:
-      help_embed.add_field(name = command, value = client.get_command(command).help)
+        help_embed.add_field(name=command, value=client.get_command(command).help)
 
     else:
-      help_embed.add_field(name = "Invalid command!", value = "That command isn't available.")
+        help_embed.add_field(
+            name="Invalid command!", value="That command isn't available."
+        )
 
-    await ctx.send(embed = help_embed)
+    await ctx.send(embed=help_embed)
 
-@slash.slash(name = "help", description = "Shows a list of the bot's commands.")
-async def _help(ctx, command = None):
-    help_embed = discord.Embed(title = "Let me help you!", color = 0x1e90ff)
-    help_embed.set_author(name = "LinerlyBot", url = "https://linerly.github.io/linerlybot", icon_url = "https://linerly.github.io/assets/linerlybot/linerlybot.png")
+
+@slash.slash(name="help", description="Shows a list of the bot's commands.")
+async def _help(ctx, command=None):
+    help_embed = discord.Embed(title="Let me help you!", color=0x1E90FF)
+    help_embed.set_author(
+        name="LinerlyBot",
+        url="https://linerly.github.io/linerlybot",
+        icon_url="https://linerly.github.io/assets/linerlybot/linerlybot.png",
+    )
     command_names_list = [x.name for x in client.commands]
 
     if not command:
-      help_embed.add_field(name = "Bot Commands", value = "\n".join([x.name for i, x in enumerate(client.commands)]), inline = False)
-      help_embed.add_field(name = "Details", value = "Type `/help <command name>` for more details about each command. \n \n*This is a rewritten version of LinerlyBot using Discord.py.*", inline = False)
+        help_embed.add_field(
+            name="Bot Commands",
+            value="\n".join([x.name for i, x in enumerate(client.commands)]),
+            inline=False,
+        )
+        help_embed.add_field(
+            name="Details",
+            value="Type `/help <command name>` for more details about each command. \n \n*This is a rewritten version of LinerlyBot using Discord.py.*",
+            inline=False,
+        )
 
     elif command in command_names_list:
-      help_embed.add_field(name = command, value = client.get_command(command).help)
+        help_embed.add_field(name=command, value=client.get_command(command).help)
 
     else:
-      help_embed.add_field(name = "Invalid command!", value = "That command isn't available.")
+        help_embed.add_field(
+            name="Invalid command!", value="That command isn't available."
+        )
 
-    await ctx.send(embed = help_embed)
+    await ctx.send(embed=help_embed)
 
-@client.command(help = "Tells you about the bot.")
+
+@client.command(help="Tells you about the bot.")
 async def about(ctx):
-    embed = discord.Embed(color = 0x1e90ff)
-    embed.set_author(name = "LinerlyBot", url = "https://linerly.github.io/linerlybot", icon_url = "https://linerly.github.io/assets/linerlybot/linerlybot.png")
-    embed.add_field(name = "A new version of LinerlyBot which uses Discord.py instead of Discord.js.", value = "Previous LinerlyBot commands will be added back in the rewritten version! \n \n [Website](https://linerly.github.io/linerlybot) \n [Add LinerlyBot to your Discord Server](https://discord.com/api/oauth2/authorize?client_id=529566778293223434&permissions=1576266871&scope=applications.commands%20bot) \n [Source Code](https://github.com/Linerly/linerlybot-rewritten)")
-    await ctx.send(embed = embed)
+    embed = discord.Embed(color=0x1E90FF)
+    embed.set_author(
+        name="LinerlyBot",
+        url="https://linerly.github.io/linerlybot",
+        icon_url="https://linerly.github.io/assets/linerlybot/linerlybot.png",
+    )
+    embed.add_field(
+        name="A new version of LinerlyBot which uses Discord.py instead of Discord.js.",
+        value="Previous LinerlyBot commands will be added back in the rewritten version! \n \n [Website](https://linerly.github.io/linerlybot) \n [Add LinerlyBot to your Discord Server](https://discord.com/api/oauth2/authorize?client_id=529566778293223434&permissions=1576266871&scope=applications.commands%20bot) \n [Source Code](https://github.com/Linerly/linerlybot-rewritten)",
+    )
+    await ctx.send(embed=embed)
 
-@slash.slash(name = "about", description = "Tells you about the bot.")
+
+@slash.slash(name="about", description="Tells you about the bot.")
 async def _about(ctx):
-    embed = discord.Embed(color = 0x1e90ff)
-    embed.set_author(name = "LinerlyBot", url = "https://linerly.github.io/linerlybot", icon_url = "https://linerly.github.io/assets/linerlybot/linerlybot.png")
-    embed.add_field(name = "A new version of LinerlyBot which uses Discord.py instead of Discord.js.", value = "Previous LinerlyBot commands will be added back in the rewritten version! \n \n [Website](https://linerly.github.io/linerlybot) \n [Add LinerlyBot to your Discord Server](https://discord.com/oauth2/authorize?client_id=529566778293223434&permissions=2147485696&scope=bot+applications.commands) \n [Source Code](https://github.com/Linerly/linerlybot-rewritten)")
-    await ctx.send(embed = embed)
+    embed = discord.Embed(color=0x1E90FF)
+    embed.set_author(
+        name="LinerlyBot",
+        url="https://linerly.github.io/linerlybot",
+        icon_url="https://linerly.github.io/assets/linerlybot/linerlybot.png",
+    )
+    embed.add_field(
+        name="A new version of LinerlyBot which uses Discord.py instead of Discord.js.",
+        value="Previous LinerlyBot commands will be added back in the rewritten version! \n \n [Website](https://linerly.github.io/linerlybot) \n [Add LinerlyBot to your Discord Server](https://discord.com/oauth2/authorize?client_id=529566778293223434&permissions=2147485696&scope=bot+applications.commands) \n [Source Code](https://github.com/Linerly/linerlybot-rewritten)",
+    )
+    await ctx.send(embed=embed)
 
-@client.command(help = "Checks the bot's current latency.")
+
+@client.command(help="Checks the bot's current latency.")
 async def ping(ctx):
     ping = str(round(client.latency * 1000))
-    embed = discord.Embed(title = "Pong!", description = ping + " ms.", color = 0x1e90ff)
-    embed.set_author(name = "LinerlyBot", url = "https://linerly.github.io/linerlybot", icon_url = "https://linerly.github.io/assets/linerlybot/linerlybot.png")
-    await ctx.send(embed = embed)
+    embed = discord.Embed(title="Pong!", description=ping + " ms.", color=0x1E90FF)
+    embed.set_author(
+        name="LinerlyBot",
+        url="https://linerly.github.io/linerlybot",
+        icon_url="https://linerly.github.io/assets/linerlybot/linerlybot.png",
+    )
+    await ctx.send(embed=embed)
 
-@slash.slash(name = "ping", description = "Checks the bot's current latency.")
+
+@slash.slash(name="ping", description="Checks the bot's current latency.")
 async def _ping(ctx):
     ping = str(round(client.latency * 1000))
-    embed = discord.Embed(title = "Pong!", description = ping + " ms.", color = 0x1e90ff)
-    embed.set_author(name = "LinerlyBot", url = "https://linerly.github.io/linerlybot", icon_url = "https://linerly.github.io/assets/linerlybot/linerlybot.png")
-    await ctx.send(embed = embed)
+    embed = discord.Embed(title="Pong!", description=ping + " ms.", color=0x1E90FF)
+    embed.set_author(
+        name="LinerlyBot",
+        url="https://linerly.github.io/linerlybot",
+        icon_url="https://linerly.github.io/assets/linerlybot/linerlybot.png",
+    )
+    await ctx.send(embed=embed)
 
-@client.command(help = "I can show you what I'm feeling now.")
+
+@client.command(help="I can show you what I'm feeling now.")
 async def feeling(ctx):
-    feelings = [':slight_smile:', ':upside_down:', ':woozy_face:', ':confused:', ':sleeping:', ':rolling_eyes:', ':smiling_face_with_tear:', ':no_mouth:']
+    feelings = [
+        ":slight_smile:",
+        ":upside_down:",
+        ":woozy_face:",
+        ":confused:",
+        ":sleeping:",
+        ":rolling_eyes:",
+        ":smiling_face_with_tear:",
+        ":no_mouth:",
+    ]
     await ctx.send(random.choice(feelings))
 
-@slash.slash(name = "feeling", description = "I can show you what I'm feeling now.")
+
+@slash.slash(name="feeling", description="I can show you what I'm feeling now.")
 async def _feeling(ctx):
-    feelings = [':slight_smile:', ':upside_down:', ':woozy_face:', ':confused:', ':sleeping:', ':rolling_eyes:', ':smiling_face_with_tear:', ':no_mouth:']
+    feelings = [
+        ":slight_smile:",
+        ":upside_down:",
+        ":woozy_face:",
+        ":confused:",
+        ":sleeping:",
+        ":rolling_eyes:",
+        ":smiling_face_with_tear:",
+        ":no_mouth:",
+    ]
     await ctx.send(random.choice(feelings))
 
-@client.command(help = "Shows some info about the bot.")
+
+@client.command(help="Shows some info about the bot.")
 async def info(ctx):
     current_time = time.time()
     difference = int(round(current_time - start_time))
-    text = str(timedelta(seconds = difference))
-  
+    text = str(timedelta(seconds=difference))
+
     ping = str(round(client.latency * 1000))
 
-    embed = discord.Embed(title = "Bot Info", color = 0x1e90ff)
-    embed.set_author(name = "LinerlyBot", url = "https://linerly.github.io/linerlybot", icon_url = "https://linerly.github.io/assets/linerlybot/linerlybot.png")
+    embed = discord.Embed(title="Bot Info", color=0x1E90FF)
+    embed.set_author(
+        name="LinerlyBot",
+        url="https://linerly.github.io/linerlybot",
+        icon_url="https://linerly.github.io/assets/linerlybot/linerlybot.png",
+    )
 
-    embed.add_field(name = "Bot Uptime", value = ":green_circle: " + text)
+    embed.add_field(name="Bot Uptime", value=":green_circle: " + text)
 
-    embed.add_field(name = "Servers", value = ":desktop: in " + str(len(client.guilds)) + " servers")
+    embed.add_field(
+        name="Servers", value=":desktop: in " + str(len(client.guilds)) + " servers"
+    )
 
-    embed.add_field(name = "Latency", value = ":globe_with_meridians: " + ping + " ms")
+    embed.add_field(name="Latency", value=":globe_with_meridians: " + ping + " ms")
 
-    embed.add_field(name = "Operating System", value = ":penguin: running on " + str(distro.name(pretty = True)))
-    await ctx.send(embed = embed)
+    embed.add_field(
+        name="Operating System",
+        value=":penguin: running on " + str(distro.name(pretty=True)),
+    )
+    await ctx.send(embed=embed)
 
-@slash.slash(name = "info", description = "Shows some info about the bot.")
+
+@slash.slash(name="info", description="Shows some info about the bot.")
 async def _info(ctx):
     current_time = time.time()
     difference = int(round(current_time - start_time))
-    text = str(timedelta(seconds = difference))
-  
+    text = str(timedelta(seconds=difference))
+
     ping = str(round(client.latency * 1000))
 
-    embed = discord.Embed(title = "Bot Info", color = 0x1e90ff)
-    embed.set_author(name = "LinerlyBot", url = "https://linerly.github.io/linerlybot", icon_url = "https://linerly.github.io/assets/linerlybot/linerlybot.png")
+    embed = discord.Embed(title="Bot Info", color=0x1E90FF)
+    embed.set_author(
+        name="LinerlyBot",
+        url="https://linerly.github.io/linerlybot",
+        icon_url="https://linerly.github.io/assets/linerlybot/linerlybot.png",
+    )
 
-    embed.add_field(name = "Bot Uptime", value = ":green_circle: " + text)
+    embed.add_field(name="Bot Uptime", value=":green_circle: " + text)
 
-    embed.add_field(name = "Servers", value = ":desktop: in " + str(len(client.guilds)) + " servers")
+    embed.add_field(
+        name="Servers", value=":desktop: in " + str(len(client.guilds)) + " servers"
+    )
 
-    embed.add_field(name = "Latency", value = ":globe_with_meridians: " + ping + " ms")
+    embed.add_field(name="Latency", value=":globe_with_meridians: " + ping + " ms")
 
-    embed.add_field(name = "Operating System", value = ":penguin: running on " + str(distro.name(pretty = True)))
-    await ctx.send(embed = embed)
+    embed.add_field(
+        name="Operating System",
+        value=":penguin: running on " + str(distro.name(pretty=True)),
+    )
+    await ctx.send(embed=embed)
 
-@client.command(help = "Tells you random jokes.")
+
+@client.command(help="Tells you random jokes.")
 async def joke(ctx):
     await ctx.send(pyjokes.get_joke())
 
-@slash.slash(name = "joke", description = "Tells you random jokes.")
+
+@slash.slash(name="joke", description="Tells you random jokes.")
 async def _joke(ctx):
     await ctx.send(pyjokes.get_joke())
 
-@client.command(help = "Tells you random quotes.")
+
+@client.command(help="Tells you random quotes.")
 async def quote(ctx):
-    async with ctx.typing(): 
+    async with ctx.typing():
         await ctx.send(Quote.print())
 
-@slash.slash(name = "quote", description = "Tells you random quotes.")
+
+@slash.slash(name="quote", description="Tells you random quotes.")
 async def _quote(ctx):
     await ctx.send(Quote.print())
 
-@client.command(help = "Generate text based on an input sentence, predict the next text from partial sentences, or just complete a paragraph of a text. The generated text *may* contain profanity.")
-async def text(ctx, text = None):
-    r = requests.post(
-    "https://api.deepai.org/api/text-generator",
-    data={
-        'text': text,
-    },
-    headers={'api-key': os.environ['DEEPAI_API_KEY']}
+
+@client.command(
+    help="Generate text based on an input sentence, predict the next text from partial sentences, or just complete a paragraph of a text. The generated text *may* contain profanity."
 )
+async def text(ctx, text=None):
+    r = requests.post(
+        "https://api.deepai.org/api/text-generator",
+        data={
+            "text": text,
+        },
+        headers={"api-key": os.environ["DEEPAI_API_KEY"]},
+    )
 
     await ctx.send(f"Raw JSON output \n \n ```{str(r.json())}```")
 
-@slash.slash(name = "text", description = "Generate text based on an input sentence, predict the next text from partial sentences, or just complete a paragraph of a text. The generated text *may* contain profanity.")
-async def _text(ctx, text = None):
-    r = requests.post(
-    "https://api.deepai.org/api/text-generator",
-    data={
-        'text': text,
-    },
-    headers={'api-key': os.environ['DEEPAI_API_KEY']}
+
+@slash.slash(
+    name="text",
+    description="Generate text based on an input sentence, predict the next text from partial sentences, or just complete a paragraph of a text. The generated text *may* contain profanity.",
 )
+async def _text(ctx, text=None):
+    r = requests.post(
+        "https://api.deepai.org/api/text-generator",
+        data={
+            "text": text,
+        },
+        headers={"api-key": os.environ["DEEPAI_API_KEY"]},
+    )
 
     await ctx.send(f"Raw JSON output \n \n ```{str(r.json())}```")
+
 
 keep_alive()
-client.run(os.environ['TOKEN'])
+client.run(os.environ["TOKEN"])
